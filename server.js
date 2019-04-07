d39849c5a3134e6434fb50e9f3a3bd493af97cfa
Kuba Fogel <kuba.fogel@movecloser.pl>
2019-04-07 16:51:27 +0200

    fix

--- server.js
+++ server.js
@@ -11,36 +11,6 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
-// app.get('/abc', (req, res) => {
-//     console.log('Server GET Abc');
-//
-//     if (req.query.a == null) {
-//         return res.status(400).end('Error');
-//     }
-//
-//     console.log(req.query.a);
-//
-//     res.status(200);
-//     res.write('/GET ABC');
-//     res.write('A: ' + req.query.a);
-//
-//     res.status(201).end()
-// });
-//
-// app.get('/abc/:id', (req, res) => {
-//     console.log(req.params);
-//     res.end(req.params.id)
-// });
-//
-// app.post('/abc', (req, res) => {
-//     console.log('Server POST Abc');
-//
-//     res.status(200);
-//     console.log(req.body);
-//     res.end('/POST ABC')
-// });
-
-
 const data = {
     "1": {
         "name": "Stephen"
